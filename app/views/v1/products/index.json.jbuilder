0527d2b94df601f01ff986de7b627d97cfd9748b
Ciakarellas <subskrypcje.maciek@gmail.com>
2022-01-19 15:24:18 +0100

    adding new field id

--- app/views/v1/products/index.json.jbuilder
+++ app/views/v1/products/index.json.jbuilder
@@ -1,4 +1,5 @@
 json.products @products do |product|
      json.name product.name
      json.done product.done
+     json.id product.id
 end
--- db/migrate/20211207054404_create_products.rb
+++ db/migrate/20211207054404_create_products.rb
@@ -3,6 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :name
       t.boolean :done
+      t.integer :id
 
       t.timestamps
     end
